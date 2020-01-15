56efa78a6e91051f5cc623db3ea98e2c59df49b9
AAmanbekov <aamanbekov@beeline.kz>
2020-01-15 18:55:38 +0600

    singleHash looks like done

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"runtime"
 	"strconv"
 	"time"
 )
 
 // сюда писать код
 
+var in chan interface{}
+var out chan interface{}
+
 func main() {
+	in = make(chan interface{}, 1)
+	out = make(chan interface{}, 1)
+	reader := bufio.NewReader(os.Stdin)
+	arg, _ := reader.ReadString('\n')
+
+	fmt.Println(arg)
 	start := time.Now()
-	ch1 := make(chan interface{})
-	ch2 := make(chan interface{})
-	go SingleHash(ch1, ch2)
-	ch1 <- "0"
-	fmt.Printf("%v", <-ch2)
+	go SingleHash(in, out)
+	//go MultiHash(in, out)
+	in <- "0"
+	//for r := range out {
+	//	log.Println(r)
+	//}
+
+	//fmt.Println(<-in)
+	//fmt.Println(<-out)
 	end := time.Since(start)
 	//go MultiHash(ch2, ch1)
 	//fmt.Printf("%v\n", <-ch2)
@@ -29,33 +45,31 @@ func main() {
 	//MultiHash(data)
 }
 
-func ExecutePipeLine() {
-
+func ExecutePipeLine(pipeline []job) {
+	for _, f := range pipeline {
+		go f(in, out)
+	}
 }
 
 var SingleHash = func(in, out chan interface{}) {
-	var result string
-	ch1 := make(chan interface{}, 1)
-	dataRaw := <-in
-	data, ok := dataRaw.(string)
+	data, ok := (<-in).(string)
 
 	if !ok {
 		fmt.Println("cant convert result data to string")
 	}
 
-	go func(out chan <- interface{}) {
-		out <- DataSignerCrc32(data)
+	ch1 := make(chan string, 1)
+
+	go func(ch1 chan string){
+		ch1 <- DataSignerCrc32(DataSignerMd5(data))
 	}(ch1)
 
-	go func(out chan <- interface{}) {
-		out <- DataSignerCrc32(DataSignerMd5(data))
-		close(out)
+	go func(ch1 chan string){
+		ch1 <- DataSignerCrc32(data)
+		runtime.Gosched()
 	}(ch1)
 
-	for i := range ch1 {
-		result += fmt.Sprintf("%v~", i)
-	}
-
+	out <- fmt.Sprintf("%v~%v", <-ch1, <-ch1)
 	//out <- DataSignerCrc32(data) + "~" +
 }
 
@@ -70,7 +84,6 @@ var MultiHash = func(in, out chan interface{}) {
 	for i := 0; i <= 5; i++ {
 		go func(out chan <- interface{}) {
 			out <- DataSignerCrc32(strconv.Itoa(i) + data)
-			//out <- tmp
 		}(out)
 		fmt.Printf("%v MultiHash: crc32(th+step1)) %v %v\n", data, i, <-out)
 		//result += fmt.Sprintf("%v", tmp)
